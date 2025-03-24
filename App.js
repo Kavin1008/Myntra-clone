a531ba59a41c512e693c95eadf5483e5e82f64b5
Kavin1008 <kavin38502@gmail.com>
2025-03-24 10:44:12 +0530

    cmmit

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Route from './routes/Route';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 export default function App() {
